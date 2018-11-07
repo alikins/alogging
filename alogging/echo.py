5f31587550b914b2c587202d0a2b0306c513148b
Adrian Likins <alikins@redhat.com>
2018-11-07 14:58:32 -0500

    raw strings for regex params

--- alogging/echo.py
+++ alogging/echo.py
@@ -13,7 +13,7 @@ def echo_format(value, depth=1, caller_name='echo_format'):
     frame = inspect.getframeinfo(calling_frame_record[0])
     out = ''
     if frame.code_context:
-        m = re.search("%s\((.+)\)" % caller_name, frame.code_context[0])
+        m = re.search(r"%s\((.+)\)" % caller_name, frame.code_context[0])
         if m:
             out = "{0} = {1}".format(m.group(1), pf(value))
     return out
