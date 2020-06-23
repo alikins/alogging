5839dd1c74daf0a6ac27d92d7f298820c50538a2
Adrian Likins <alikins@redhat.com>
2020-06-23 14:41:34 -0400

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='alogging',
-    version='0.4.3',
+    version='0.4.4',
     description="A collection of logging utilities",
     long_description=readme,
     author="Adrian Likins",
